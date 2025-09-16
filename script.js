826d1075585ea224f3c28be3feb7bdc6c28ddb7f
aisalkyn4160 <aisalkyn.4160@gmail.com>
2025-09-16 14:23:30 +0700

    adding about, documents, news, feedback blocks

--- script.js
+++ script.js
@@ -14,4 +14,43 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     
 });
+
+ const aboutSwiper = new Swiper('.about-swiper', {
+        slidesPerView: 3,
+        spaceBetween: 24,
+        pagination: {
+            el: '.about-swiper-pagination',
+           type: 'fraction',
+           
+        },
+
+        navigation: {
+            nextEl: '.about-btn-next',
+            prevEl: '.about-btn-prev',
+        },
+    
 });
+
+const documentsSwiper = new Swiper('.documents-swiper', {
+        slidesPerView: 4.5,
+        spaceBetween: 24,
+        loop: true,
+         navigation: {
+            nextEl: '.documents .swiper-btn-next',
+            prevEl: '.documents .swiper-btn-prev',
+        },
+    
+});
+
+});
+
+$(function () {
+    $('.news-tab:first-child').addClass('active')
+    $('.news-items:first-child').addClass('active')
+    $('.news-tab').click(function () {
+        $('.news-tab').removeClass('active');
+        $('.news-items').removeClass('active');
+        $(this).addClass('active');
+        $(`.news-items[data-tab="${this.dataset.tab}"]`).addClass('active');
+    });
+})
